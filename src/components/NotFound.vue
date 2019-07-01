eceb98d69886b2b7b4b24b476e397a01c535025e
Martin Heinz <martin.heinz1@ibm.com>
2019-07-01 10:35:35 +0200

    Add Not Found (404) endpoint.

---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>The Page you are looking for is not there...</h1>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../components/Home.vue';
 import Blog from '../components/Blog.vue';
+import NotFound from '../components/NotFound.vue';
 
 Vue.use(Router);
 
@@ -11,5 +12,7 @@ export default new Router({
         { path: '/', component: Home },
         { path: '/blog/:id', component: Blog },
         // { path: '/contact', component: 'Contact' },
+        { path: '/404', component: NotFound },
+        { path: '*', redirect: '/404' },
     ],
 });
