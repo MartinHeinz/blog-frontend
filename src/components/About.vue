4a408630fbddb6321ebe898779d325664886683b
Martin Heinz <martin.heinz1@ibm.com>
2019-07-24 19:35:34 +0200

    About page content.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="column">
         <h1>About Me</h1>
-        <p>Some text...</p>
+        <p>My name is Martin Heinz and I'm a software developer/DevOps engineer, currently working at IBM.
+            You can find me on
+            <a class="no-underline" href="https://github.com/MartinHeinz" target="_blank">
+            <v-hover>
+                <v-icon @mouseover="next_active = true" @mouseout="next_active = false"
+                        slot-scope="{ hover }"
+                        :class="`${hover? 'icon-active': 'icon-inactive'}`">fab fa-github</v-icon>
+            </v-hover>
+            </a>
+            or on
+            <a class="no-underline" href="https://www.linkedin.com/in/heinz-martin" target="_blank">
+            <v-hover>
+                <v-icon @mouseover="next_active = true" @mouseout="next_active = false"
+                        slot-scope="{ hover }"
+                        :class="`${hover? 'icon-active': 'icon-inactive'}`">fab fa-linkedin</v-icon>
+            </v-hover>
+            </a>
+            </p>
     </div>
 </template>
 
@@ -11,6 +28,18 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+    .custom-btn::before {
+        color: transparent
+    }
+
+    .custom-btn:hover {
+        color: #d480aa;
+        min-width: 0;
+    }
+
+    .no-underline {
+        background-image: none !important;
+    }
 
 </style>
